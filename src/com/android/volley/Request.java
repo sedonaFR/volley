7853177796fd5f141865288b110bf4d08a2a5a94
bdelville <bdelville@sedona.fr>
2014-06-10 11:11:28 +0200

    correctif cache post

--- src/com/android/volley/Request.java
+++ src/com/android/volley/Request.java
@@ -296,7 +296,7 @@ public abstract class Request<T> implements Comparable<Request<T>> {
      * Returns the cache key for this request.  By default, this is the URL.
      */
     public String getCacheKey() {
-        return getUrl();
+        return getUrl() + getMethod();
     }
 
     /**
